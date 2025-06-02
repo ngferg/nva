d665bc56f6f737997401b02afec97c2da8eb162a
ngferg <me@nicholaus-ferguson.us>
2025-06-01 22:40:19 -0500

    little refactor

--- nva.py
+++ nva.py
@@ -4,7 +4,6 @@
 # Example usage using Dutch (nl) recognition model: `python test_microphone.py -m nl`
 # For more help run: `python test_microphone.py -h`
 
-import argparse
 import queue
 import sys
 import pyttsx3
@@ -15,17 +14,9 @@ from vosk import Model, KaldiRecognizer
 
 from ask import Ask
 from intents import Intent
-
-
+import parser
 
 q = queue.Queue()
-
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
 
 def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
@@ -33,33 +24,7 @@ def callback(indata, frames, time, status):
         print(status, file=sys.stderr)
     q.put(bytes(indata))
 
-
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    "-l", "--list-devices", action="store_true",
-    help="show list of audio devices and exit")
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    "-f", "--filename", type=str, metavar="FILENAME",
-    help="audio file to store recording to")
-parser.add_argument(
-    "-d", "--device", type=int_or_str,
-    help="input device (numeric ID or substring)")
-parser.add_argument(
-    "-r", "--samplerate", type=int, help="sampling rate")
-parser.add_argument(
-    "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
-parser.add_argument(
-    "-p", "--partials", type=bool, help="log partial recordings"
-)
-args = parser.parse_args(remaining)
+args = parser.get_args()
 
 try:
     partials = args.partials
@@ -78,47 +43,46 @@ try:
     else:
         dump_fn = None
 
-    stream = sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,
-            dtype="int16", channels=1, callback=callback)
-    print("#" * 80)
-    print("Press Ctrl+C to stop the recording")
-    print("#" * 80)
-    
-    stay_active = True
+    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,
+            dtype="int16", channels=1, callback=callback) as stream:
+        print("#" * 80)
+        print("Press Ctrl+C to stop the recording")
+        print("#" * 80)
+        
+        stay_active = True
 
 
-    tts = pyttsx3.init()
-    stream.stop()
-    tts.say("Howdy")
-    tts.runAndWait()
-    stream.start()
+        tts = pyttsx3.init()
+        stream.stop()
+        tts.say("Howdy")
+        tts.runAndWait()
+        stream.start()
 
-    rec = KaldiRecognizer(model, args.samplerate)
+        rec = KaldiRecognizer(model, args.samplerate)
 
-    while stay_active:
-        data = q.get()
-    
-        if rec.AcceptWaveform(data):
-            utt = json.loads(rec.Result())['text']
-            if utt.__len__() > 0:
-                ask = Ask(utt)
-                ask.debug_print()
+        while stay_active:
+            data = q.get()
+        
+            if rec.AcceptWaveform(data):
+                utt = json.loads(rec.Result())['text']
+                if utt.__len__() > 0:
+                    ask = Ask(utt)
+                    ask.debug_print()
 
-                if ask.intent == Intent.EXIT:
-                    stay_active = False
-                
-                ask.skill.execute_ask(ask)
+                    if ask.intent == Intent.EXIT:
+                        stay_active = False
+                    
+                    ask.skill.execute_ask(ask)
 
-                if ask.talkback.__len__() > 0:
-                    stream.stop()
-                    tts.say(ask.talkback)
-                    tts.runAndWait()
-                    stream.start()
-        else:
-            if partials: print(rec.PartialResult())
-        if dump_fn is not None:
-            dump_fn.write(data)
-    stream.close()
+                    if ask.talkback.__len__() > 0:
+                        stream.stop()
+                        tts.say(ask.talkback)
+                        tts.runAndWait()
+                        stream.start()
+            else:
+                if partials: print(rec.PartialResult())
+            if dump_fn is not None:
+                dump_fn.write(data)
 
 except KeyboardInterrupt:
     print("\nDone")
---
+++ parser.py
@@ -0,0 +1,39 @@
+
+import argparse
+import sounddevice as sd
+
+
+def get_args():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-l", "--list-devices", action="store_true",
+        help="show list of audio devices and exit")
+    args, remaining = parser.parse_known_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        parser.exit(0)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[parser])
+    parser.add_argument(
+        "-f", "--filename", type=str, metavar="FILENAME",
+        help="audio file to store recording to")
+    parser.add_argument(
+        "-d", "--device", type=int_or_str,
+        help="input device (numeric ID or substring)")
+    parser.add_argument(
+        "-r", "--samplerate", type=int, help="sampling rate")
+    parser.add_argument(
+        "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
+    parser.add_argument(
+        "-p", "--partials", type=bool, help="log partial recordings"
+    )
+    return parser.parse_args(remaining)
+
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
